ef4e42571a4e8e48f88d744e359cb3f32793f8f9
Henry Haustein <henry.haustein@web.de>
2024-08-26 11:05:01 +0200

    fix import

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from expansion_methods.gc_expansion import gram_charlier_expansion
+from expansion_methods.expansion_methods import gram_charlier_expansion
 
 x = np.linspace(-3, 3, 100)
 y = gram_charlier_expansion(x, 0, 1, 0, 0)
