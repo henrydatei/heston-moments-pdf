6fab25b4e9c05dc9c27974bb2fada98c100b460c
henrydatei <31316025+henrydatei@users.noreply.github.com>
2024-11-14 15:29:20 +0100

    noch mehr checks für haozhe

--- symbolic_computation/check_noncentral_central_moments.py
+++ symbolic_computation/check_noncentral_central_moments.py
@@ -7,6 +7,9 @@ mu, theta, kappa, rho, sigma, t = sp.symbols('mu theta kappa rho sigma t')
 
 # Define mu_1 and mu_2
 mu_1 = (mu - theta / 2) * t
+
+Ert1 = (mu - theta/2)*t
+
 mu_2 = (1 / (4 * kappa**3)) * (
     sp.exp(-kappa * t) * (
         sp.exp(kappa * t) * (
@@ -18,6 +21,7 @@ mu_2 = (1 / (4 * kappa**3)) * (
         + sigma * theta * (sigma - 4 * kappa * rho)
     )
 )
+
 Ert2 = (sigma * (- 4 * kappa * rho + sigma) * theta + sp.exp(kappa * t) * (- (sigma**2 * theta) - 4 * kappa**2 * rho * sigma * t * theta + kappa * sigma * (4 * rho + sigma * t) * theta + kappa**3 * t * (4 * theta + t * (-2 * mu + theta)**2))) / (4 * sp.exp(kappa * t) * kappa**3)
 
 mu_3 = (sp.exp(-kappa * t))/(8*kappa**5) * (
@@ -46,12 +50,8 @@ mu_3 = (sp.exp(-kappa * t))/(8*kappa**5) * (
         + 2 * sigma**3
     )
 )
-Ert3 =  (- 3 * sigma * theta * (2 * sigma**3 + kappa * sigma**2 * (- 12 * rho + sigma * t) + 4 * kappa**3 * rho*(- 2 + 2 * mu * t + 2 * rho * sigma * t - t * theta) + 
-kappa**2 * sigma * (4 + 16 * rho**2 - 2 * mu * t - 6 * rho * sigma * t + t * theta)) + 
-sp.exp(kappa * t) * (6 * sigma**4 * theta - 3 * kappa * sigma**3 * (12 * rho + sigma * t) * theta + 12 * kappa**4 * rho * sigma * t * theta * (2 - 2 * mu * t + t * theta) + 
-3 * kappa**2 * sigma**2 * theta * (4 + 16 * rho**2 - 2 * mu * t + 6 * rho * sigma * t + t * theta) - 
-3 * kappa**3 * sigma * theta * (8 * rho**2 * sigma * t + sigma * t * (4 - 2 * mu * t + t * theta) + rho * (8 - 8 * mu * t + 4 * t * theta)) + 
-kappa**5 * t * (t * (2 * mu - theta) * (12 * theta + t * (- 2 * mu + theta)**2)))) / (8 * sp.exp(kappa * t) * kappa**5)
+
+Ert3 =  (- 3 * sigma * theta * (2 * sigma**3 + kappa * sigma**2 * (- 12 * rho + sigma * t) + 4 * kappa**3 * rho*(- 2 + 2 * mu * t + 2 * rho * sigma * t - t * theta) + kappa**2 * sigma * (4 + 16 * rho**2 - 2 * mu * t - 6 * rho * sigma * t + t * theta)) + sp.exp(kappa * t) * (6 * sigma**4 * theta - 3 * kappa * sigma**3 * (12 * rho + sigma * t) * theta + 12 * kappa**4 * rho * sigma * t * theta * (2 - 2 * mu * t + t * theta) + 3 * kappa**2 * sigma**2 * theta * (4 + 16 * rho**2 - 2 * mu * t + 6 * rho * sigma * t + t * theta) - 3 * kappa**3 * sigma * theta * (8 * rho**2 * sigma * t + sigma * t * (4 - 2 * mu * t + t * theta) + rho * (8 - 8 * mu * t + 4 * t * theta)) + kappa**5 * t * (t * (2 * mu - theta) * (12 * theta + t * (- 2 * mu + theta)**2)))) / (8 * sp.exp(kappa * t) * kappa**5)
 
 mu_4 = (1 / (32 * kappa**7)) * (
     2 * kappa * t * (
@@ -95,19 +95,8 @@ mu_4 = (1 / (32 * kappa**7)) * (
         ) + (sigma - 4 * kappa * rho)**2 * (2 * kappa * theta + sigma**2)
     )
 )
-Ert4 = (( - 24 * kappa**2 * (2 * kappa * rho - sigma) * sigma * t * (2 * (- 1 + sp.exp(kappa * t)) * sigma**2 - kappa * sigma * (8 * (- 1 + sp.exp(kappa * t)) * rho + sigma * t) 
-+ 4 * kappa**2 * (- 1 + sp.exp(kappa * t) + rho * sigma * t)) * (2 * mu - theta) * theta)/sp.exp(kappa * t) + (3 * sigma**2 * theta * ((-4 * kappa*rho + sigma)**2 * 
-(sigma**2 + 2 * kappa * theta) + sp.exp(2 * kappa * t) * (- 32 * kappa**4 * (1 + 8 * rho**2) - 29 * sigma**4 + 2 * kappa * sigma**2 * (116 * rho * sigma + theta) - 
-16 * kappa**2 * sigma * (6 * sigma + 35 * rho**2 * sigma + rho * theta) + 32 * kappa**3 * rho * (12 * (1 + rho**2) * sigma + rho * theta)) - 
-4 * sp.exp(kappa * t) * (- 7 * sigma**4 + 16 * kappa**5 * rho**2 * t * (-2 + rho * sigma * t) + 4 * kappa**4 * (- 2 + rho * (12 * sigma*t +
-rho * (- 16 + sigma * t * (16 * rho - 5 * sigma * t)))) + kappa * sigma**2 * (56 * rho * sigma - 5 * sigma**2 * t + theta) - 
-kappa**2 * sigma * (sigma * (24 + 136 * rho**2 - 40 * rho * sigma * t + sigma**2 * t**2) + 8 * rho * theta) + 
-4 * kappa**3 * (sigma * (24 * (rho + rho**3) - 3 * (1 + 8 * rho**2) * sigma * t + 2 * rho * sigma**2 * t**2) + 4 * rho**2 * theta)))) / sp.exp(2 * kappa * t) + (12 * (- 1 + sp.exp(kappa * t)) * kappa**2 * (4 * kappa * rho - sigma) * sigma * t * theta * (- 4 * kappa * rho * sigma * theta + 
-sigma**2 * theta + kappa**2 * (4 * theta + t * (- 2 * mu + theta)**2))) / sp.exp(kappa * t) + 
-2 * kappa * t * (- 120 * kappa * rho * sigma**5 * theta + 15 * sigma**6 * theta - 48 * kappa**3 * rho * sigma**3 * theta * (6 + 4 * rho**2 - 3 * mu * t + 2 * t * theta) + 
-3 * kappa**2 * sigma**4 * theta * (24 + 96 * rho**2 - 8 * mu * t + 5 * t * theta) + kappa**6 * (t * (16 * mu**4 * t**2 - 32 * mu**2 * t * (- 3 + mu * t) * theta +
-24 * (2 + mu * t * (- 4 + mu * t)) * theta**2 - 8 * t * (- 3 + mu * t) * theta**3 + t**2 * theta**4)) - 24 * kappa**5 * rho * sigma * t * theta * (4 * mu**2 * t - 4 * mu * (2 + t * theta) + theta * (8 + t * theta)) + 
-6 * kappa**4 * sigma**2 * theta * (8 - 16 * mu * t + 8 * rho**2 * (4 - 4 * mu * t + 3 * t * theta) + t * (12 * theta + t * (- 2 * mu + theta)**2)))) / (32 * kappa**7)
+
+Ert4 = (( - 24 * kappa**2 * (2 * kappa * rho - sigma) * sigma * t * (2 * (- 1 + sp.exp(kappa * t)) * sigma**2 - kappa * sigma * (8 * (- 1 + sp.exp(kappa * t)) * rho + sigma * t) + 4 * kappa**2 * (- 1 + sp.exp(kappa * t) + rho * sigma * t)) * (2 * mu - theta) * theta)/sp.exp(kappa * t) + (3 * sigma**2 * theta * ((-4 * kappa*rho + sigma)**2 * (sigma**2 + 2 * kappa * theta) + sp.exp(2 * kappa * t) * (- 32 * kappa**4 * (1 + 8 * rho**2) - 29 * sigma**4 + 2 * kappa * sigma**2 * (116 * rho * sigma + theta) - 16 * kappa**2 * sigma * (6 * sigma + 35 * rho**2 * sigma + rho * theta) + 32 * kappa**3 * rho * (12 * (1 + rho**2) * sigma + rho * theta)) - 4 * sp.exp(kappa * t) * (- 7 * sigma**4 + 16 * kappa**5 * rho**2 * t * (-2 + rho * sigma * t) + 4 * kappa**4 * (- 2 + rho * (12 * sigma*t + rho * (- 16 + sigma * t * (16 * rho - 5 * sigma * t)))) + kappa * sigma**2 * (56 * rho * sigma - 5 * sigma**2 * t + theta) - kappa**2 * sigma * (sigma * (24 + 136 * rho**2 - 40 * rho * sigma * t + sigma**2 * t**2) + 8 * rho * theta) + 4 * kappa**3 * (sigma * (24 * (rho + rho**3) - 3 * (1 + 8 * rho**2) * sigma * t + 2 * rho * sigma**2 * t**2) + 4 * rho**2 * theta)))) / sp.exp(2 * kappa * t) + (12 * (- 1 + sp.exp(kappa * t)) * kappa**2 * (4 * kappa * rho - sigma) * sigma * t * theta * (- 4 * kappa * rho * sigma * theta + sigma**2 * theta + kappa**2 * (4 * theta + t * (- 2 * mu + theta)**2))) / sp.exp(kappa * t) + 2 * kappa * t * (- 120 * kappa * rho * sigma**5 * theta + 15 * sigma**6 * theta - 48 * kappa**3 * rho * sigma**3 * theta * (6 + 4 * rho**2 - 3 * mu * t + 2 * t * theta) + 3 * kappa**2 * sigma**4 * theta * (24 + 96 * rho**2 - 8 * mu * t + 5 * t * theta) + kappa**6 * (t * (16 * mu**4 * t**2 - 32 * mu**2 * t * (- 3 + mu * t) * theta + 24 * (2 + mu * t * (- 4 + mu * t)) * theta**2 - 8 * t * (- 3 + mu * t) * theta**3 + t**2 * theta**4)) - 24 * kappa**5 * rho * sigma * t * theta * (4 * mu**2 * t - 4 * mu * (2 + t * theta) + theta * (8 + t * theta)) + 6 * kappa**4 * sigma**2 * theta * (8 - 16 * mu * t + 8 * rho**2 * (4 - 4 * mu * t + 3 * t * theta) + t * (12 * theta + t * (- 2 * mu + theta)**2)))) / (32 * kappa**7)
 
 # Calculate the zetas
 zeta_2_check = mu_2 - mu_1**2
@@ -142,6 +131,9 @@ zeta_3 = (
     ) 
     + 2 * sigma**2 * (sp.exp(kappa * t) - 1)
 )
+
+skewrt = (- 3 * (2 * kappa * rho - sigma) * sigma * (- 2 * sigma**2 + kappa * sigma * (8 * rho - sigma * t) + 4 * kappa**2 * (- 1 + rho * sigma * t)) * theta - sp.exp(kappa * t) * (- 3 * (2 * kappa * rho - sigma) * sigma * (- 2 * sigma**2 + 4 * kappa**3 * t + kappa * sigma * (8 * rho + sigma * t) - 4 * kappa**2 * (1 + rho * sigma * t)) * theta + 12 * kappa**5 * 0 * t * 0**2 + kappa**5 * 0 * t * 0**3) + 2 * sp.exp(kappa * t) * kappa**5 * 0 * t * sp.log(1 + 0) * (3 * 0 * (4 + 0) - 6 * 0 * sp.log(1 + 0) + 4 * sp.log(1 + 0)**2)) / (sp.exp(kappa * t) * kappa**5 * (((- 1 + sp.exp(- kappa * t)) * sigma**2 * theta - 4 * kappa**2 * rho * sigma * t * theta + kappa * sigma * ((4 - 4 / sp.exp(kappa * t)) * rho + sigma * t) * theta + kappa**3 * t * (4 * theta + 0 * 0 * (4 + 0))) / kappa**3 - 4 * 0 * t * 0 * sp.log(1 + 0) + 4 * 0 * t * sp.log(1 + 0)**2)**1.5)
+
 zeta_4 = (3*sp.exp(-2*kappa*t))/(zeta_2**2) * (
     4 * sigma * sp.exp(kappa*t) * (
         4 * kappa**4 * sigma * (
@@ -205,6 +197,8 @@ zeta_4 = (3*sp.exp(-2*kappa*t))/(zeta_2**2) * (
     + sigma**2 * (sigma - 4*kappa*rho)**2 * (2*kappa*theta + sigma**2)
 )
 
+kurtrt = (3 * sigma**2 * (- 4 * kappa * rho + sigma)**2 * theta * (sigma**2 + 2 * kappa * theta) + 12 * sp.exp(kappa * t) * sigma * theta * (7 * sigma**5 - kappa * sigma**3 * (56 * rho*sigma - 5 * sigma**2 * t + theta) + kappa**2 * sigma**2 * (- 40 * rho * sigma**2 * t + sigma**3 * t**2 + 8 * rho * theta + sigma * (24 + 136 * rho**2 + t * theta)) - 4 * kappa**3 * sigma * (24 * rho**3 * sigma - 3 * sigma**2 * t + 4 * rho**2 * (- 6 * sigma**2 * t + theta) + 2 * rho * sigma * (12 + sigma**2 * t**2 + t * theta)) - 4 * kappa**5 * rho * t * (- 8 * rho * sigma + 4 * rho**2 * sigma**2 * t + 4 * theta + 0 * 0 * (4 + 0)) + kappa**4 * sigma * (8 - 48 * rho * sigma * t - 64 * rho**3 * sigma * t + 4 * rho**2 * (16 + 5 * sigma**2 * t**2 + 4 * t * theta) + t * (4 * theta + 0 * 0 * (4 + 0)))) + sp.exp(2 * kappa * t) * (- 87 * sigma**6 * theta + 6 * kappa * sigma**4 * theta * (116 * rho * sigma + 5 * sigma**2 * t + theta) + 6 * kappa**3 * sigma**2 * theta * (192 * rho**3 * sigma + 16 * rho**2 * (6 * sigma**2 * t + theta) + 16 * rho * sigma * (12 + t * theta) + sigma**2 * t * (24 + t*theta)) - 12 * kappa**2 * sigma**3 * theta * (20 * rho * sigma**2 * t + 4 * rho * theta + sigma * (24 + 140 * rho**2 + t * theta)) - 48 * kappa**6 * rho * sigma * t**2 * theta * (4 * theta + 0 * 0 * (4 + 0)) - 12 * kappa**4 * sigma**2 * theta * (8 + 32 * rho**3 * sigma * t + 16 * rho**2 * (4 + t * theta) + 4 * rho * sigma * t * (12 + t * theta) + t * (4 * theta + 0 * 0 * (4 + 0))) + 2 * kappa**7 * t * (0 * 0**2 * (48 + 24 * 0 + 0**2) + 3 * t * (4 * theta + 0 * 0 * (4 + 0))**2) + 12 * kappa**5 * sigma * t * theta * (4 * rho * (4 * theta + 0 * 0 * (4 + 0)) + sigma * (8 + 8 * rho**2 * (4 + t * theta) + t * (4 * theta + 0 * 0 * (4 + 0))))) - 16 * sp.exp(kappa * t) * kappa**4 * 0 * t * 0 * (- 3 * (- 1 + sp.exp(kappa * t)) * sigma**2 * theta - 12 * sp.exp(kappa * t) * kappa**2 * rho * sigma * t * theta + 3 * kappa * sigma * (4 * (- 1 + sp.exp(kappa * t)) * rho + sp.exp(kappa * t) * sigma * t) * theta + sp.exp(kappa * t) * kappa**3 * (0 * (12 + 0) +3 * t * (4 * theta + 0 * 0 * (4 + 0)))) * sp.log(1 + 0) + 48 * sp.exp(kappa * t) * kappa**4 * 0 * t * ((1 - sp.exp(kappa * t)) * sigma**2 * theta - 4 * sp.exp(kappa * t) * kappa**2 * rho * sigma * t * theta + kappa * sigma * (4 * (- 1 + sp.exp(kappa * t)) * rho + sp.exp(kappa * t) * sigma * t) * theta + sp.exp(kappa * t) * kappa**3 * (0 * (4 + 0) + t * (4 * theta + 0 * 0 * (4 + 3 * 0)))) * sp.log(1 + 0)**2 - 64 * sp.exp(2 * kappa * t) * kappa**7 * 0 * t * (1 + 3 * 0 * t) * 0 * sp.log(1 + 0)**3 + 32 * sp.exp(2 * kappa * t) * kappa**7 * 0 * t * (1 + 3 * 0 * t) * sp.log(1 + 0)**4) / (2 * sp.exp(2 * kappa * t) * kappa**7 * (((- 1 + sp.exp(- kappa * t)) * sigma**2 * theta - 4 * kappa**2 * rho * sigma * t * theta + kappa * sigma * ((4 - 4 / sp.exp(kappa * t)) * rho + sigma * t) * theta + kappa**3 * t * (4 * theta + 0 * 0 * (4 + 0))) / kappa**3 - 4 * 0 * t * 0 * sp.log(1 + 0) + 4 * 0 * t * sp.log(1 + 0)**2)**2)
+
 # Print result
 print("Are zeta_2 and zeta_2_check equivalent?", sp.simplify(zeta_2_check - zeta_2) == 0)
 print("Are zeta_3 and zeta_3_check equivalent?", sp.simplify(zeta_3_check - zeta_3) == 0)
@@ -213,6 +207,13 @@ print("Are zeta_4 and zeta_4_check equivalent?", sp.simplify(zeta_4_check - zeta
 print("Are mu_2 and Ert2 equivalent?", sp.simplify(mu_2 - Ert2) == 0)
 print("Are mu_3 and Ert3 equivalent?", sp.simplify(mu_3 - Ert3) == 0)
 print("Are mu_4 and Ert4 equivalent?", sp.simplify(mu_4 - Ert4) == 0)
+
+print("Are zeta_3 and Skewrt equivalent?", sp.simplify(zeta_3 - skewrt) == 0)
+print("Are zeta_4 and Kurtrt equivalent?", sp.simplify(zeta_4 - kurtrt) == 0)
+print("Are zeta_3_check and Skewrt equivalent?", sp.simplify(zeta_3_check - skewrt) == 0)
+print("Are zeta_4_check and Kurtrt equivalent?", sp.simplify(zeta_4_check - kurtrt) == 0)
+
+print(sp.simplify(skewrt - ((Ert3 - 3 * Ert1 * Ert2 + 2 * Ert1**3) / ((Ert2 - Ert1**2)**(3/2)))))
 
 # print("Are zeta_2 and zeta_2_check_Ert equivalent?", sp.simplify(zeta_2 - zeta_2_check_Ert) == 0)
 # print("Are zeta_3 and zeta_3_check_Ert equivalent?", sp.simplify(zeta_3 - zeta_3_check_Ert) == 0)
@@ -229,5 +230,5 @@ print("Are mu_4 and Ert4 equivalent?", sp.simplify(mu_4 - Ert4) == 0)
 # Numerical evaluation
 values = {mu: 0, theta: 0.19, kappa: 3, rho: -0.7, sigma: 0.4, t: 1}
 print(zeta_2.subs(values).evalf(), zeta_2_check.subs(values).evalf())
-print(zeta_3.subs(values).evalf(), zeta_3_check.subs(values).evalf())
-print(zeta_4.subs(values).evalf(), zeta_4_check.subs(values).evalf())
+print(zeta_3.subs(values).evalf(), zeta_3_check.subs(values).evalf(), skewrt.subs(values).evalf())
+print(zeta_4.subs(values).evalf(), zeta_4_check.subs(values).evalf(), kurtrt.subs(values).evalf())
