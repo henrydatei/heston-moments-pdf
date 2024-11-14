35230f3d8f6128bbd433dd244bfae3214a3b67b2
henrydatei <31316025+henrydatei@users.noreply.github.com>
2024-11-14 13:27:47 +0100

    zumindest die mu_i stimmen jetzt

--- symbolic_computation/check_noncentral_central_moments.py
+++ symbolic_computation/check_noncentral_central_moments.py
@@ -18,10 +18,12 @@ mu_2 = (1 / (4 * kappa**3)) * (
         + sigma * theta * (sigma - 4 * kappa * rho)
     )
 )
+Ert2 = (sigma * (- 4 * kappa * rho + sigma) * theta + sp.exp(kappa * t) * (- (sigma**2 * theta) - 4 * kappa**2 * rho * sigma * t * theta + kappa * sigma * (4 * rho + sigma * t) * theta + kappa**3 * t * (4 * theta + t * (-2 * mu + theta)**2))) / (4 * sp.exp(kappa * t) * kappa**3)
+
 mu_3 = (sp.exp(-kappa * t))/(8*kappa**5) * (
     sp.exp(kappa*t) * (
         3 * kappa**2 * sigma**2 * theta * (
-            -2*mu*t + 16*rho**2 + 6*rho*sigma*t + t*sigma + 4
+            -2*mu*t + 16*rho**2 + 6*rho*sigma*t + t*theta + 4
         ) 
         - 3 * kappa**3 * sigma * theta * (
             rho*(-8*mu*t + 4*t*theta + 8) + sigma*t*(-2*mu*t + theta*t + 4) + 8*rho**2*sigma*t
@@ -44,6 +46,13 @@ mu_3 = (sp.exp(-kappa * t))/(8*kappa**5) * (
         + 2 * sigma**3
     )
 )
+Ert3 =  (- 3 * sigma * theta * (2 * sigma**3 + kappa * sigma**2 * (- 12 * rho + sigma * t) + 4 * kappa**3 * rho*(- 2 + 2 * mu * t + 2 * rho * sigma * t - t * theta) + 
+kappa**2 * sigma * (4 + 16 * rho**2 - 2 * mu * t - 6 * rho * sigma * t + t * theta)) + 
+sp.exp(kappa * t) * (6 * sigma**4 * theta - 3 * kappa * sigma**3 * (12 * rho + sigma * t) * theta + 12 * kappa**4 * rho * sigma * t * theta * (2 - 2 * mu * t + t * theta) + 
+3 * kappa**2 * sigma**2 * theta * (4 + 16 * rho**2 - 2 * mu * t + 6 * rho * sigma * t + t * theta) - 
+3 * kappa**3 * sigma * theta * (8 * rho**2 * sigma * t + sigma * t * (4 - 2 * mu * t + t * theta) + rho * (8 - 8 * mu * t + 4 * t * theta)) + 
+kappa**5 * t * (t * (2 * mu - theta) * (12 * theta + t * (- 2 * mu + theta)**2)))) / (8 * sp.exp(kappa * t) * kappa**5)
+
 mu_4 = (1 / (32 * kappa**7)) * (
     2 * kappa * t * (
         6 * kappa**4 * t**2 * theta * (theta - 2 * mu)**2 * (4 * kappa**2 - 4 * kappa * rho * sigma + sigma**2)
@@ -86,11 +95,29 @@ mu_4 = (1 / (32 * kappa**7)) * (
         ) + (sigma - 4 * kappa * rho)**2 * (2 * kappa * theta + sigma**2)
     )
 )
+Ert4 = (( - 24 * kappa**2 * (2 * kappa * rho - sigma) * sigma * t * (2 * (- 1 + sp.exp(kappa * t)) * sigma**2 - kappa * sigma * (8 * (- 1 + sp.exp(kappa * t)) * rho + sigma * t) 
++ 4 * kappa**2 * (- 1 + sp.exp(kappa * t) + rho * sigma * t)) * (2 * mu - theta) * theta)/sp.exp(kappa * t) + (3 * sigma**2 * theta * ((-4 * kappa*rho + sigma)**2 * 
+(sigma**2 + 2 * kappa * theta) + sp.exp(2 * kappa * t) * (- 32 * kappa**4 * (1 + 8 * rho**2) - 29 * sigma**4 + 2 * kappa * sigma**2 * (116 * rho * sigma + theta) - 
+16 * kappa**2 * sigma * (6 * sigma + 35 * rho**2 * sigma + rho * theta) + 32 * kappa**3 * rho * (12 * (1 + rho**2) * sigma + rho * theta)) - 
+4 * sp.exp(kappa * t) * (- 7 * sigma**4 + 16 * kappa**5 * rho**2 * t * (-2 + rho * sigma * t) + 4 * kappa**4 * (- 2 + rho * (12 * sigma*t +
+rho * (- 16 + sigma * t * (16 * rho - 5 * sigma * t)))) + kappa * sigma**2 * (56 * rho * sigma - 5 * sigma**2 * t + theta) - 
+kappa**2 * sigma * (sigma * (24 + 136 * rho**2 - 40 * rho * sigma * t + sigma**2 * t**2) + 8 * rho * theta) + 
+4 * kappa**3 * (sigma * (24 * (rho + rho**3) - 3 * (1 + 8 * rho**2) * sigma * t + 2 * rho * sigma**2 * t**2) + 4 * rho**2 * theta)))) / sp.exp(2 * kappa * t) + (12 * (- 1 + sp.exp(kappa * t)) * kappa**2 * (4 * kappa * rho - sigma) * sigma * t * theta * (- 4 * kappa * rho * sigma * theta + 
+sigma**2 * theta + kappa**2 * (4 * theta + t * (- 2 * mu + theta)**2))) / sp.exp(kappa * t) + 
+2 * kappa * t * (- 120 * kappa * rho * sigma**5 * theta + 15 * sigma**6 * theta - 48 * kappa**3 * rho * sigma**3 * theta * (6 + 4 * rho**2 - 3 * mu * t + 2 * t * theta) + 
+3 * kappa**2 * sigma**4 * theta * (24 + 96 * rho**2 - 8 * mu * t + 5 * t * theta) + kappa**6 * (t * (16 * mu**4 * t**2 - 32 * mu**2 * t * (- 3 + mu * t) * theta +
+24 * (2 + mu * t * (- 4 + mu * t)) * theta**2 - 8 * t * (- 3 + mu * t) * theta**3 + t**2 * theta**4)) - 24 * kappa**5 * rho * sigma * t * theta * (4 * mu**2 * t - 4 * mu * (2 + t * theta) + theta * (8 + t * theta)) + 
+6 * kappa**4 * sigma**2 * theta * (8 - 16 * mu * t + 8 * rho**2 * (4 - 4 * mu * t + 3 * t * theta) + t * (12 * theta + t * (- 2 * mu + theta)**2)))) / (32 * kappa**7)
 
 # Calculate the zetas
 zeta_2_check = mu_2 - mu_1**2
 zeta_3_check = (mu_3 - 3 * mu_1 * mu_2 + 2 * mu_1**3) / (zeta_2_check**(3/2))
 zeta_4_check = (mu_4 - 4 * mu_1 * mu_3 + 6 * mu_1**2 * mu_2 - 3 * mu_1**4) / (zeta_2_check**2)
+
+# Calculate the zetas with Erts
+zeta_2_check_Ert = Ert2 - mu_1**2
+zeta_3_check_Ert = (Ert3 - 3 * mu_1 * Ert2 + 2 * mu_1**3) / (zeta_2_check_Ert**(3/2))
+zeta_4_check_Ert = (Ert4 - 4 * mu_1 * Ert3 + 6 * mu_1**2 * Ert2 - 3 * mu_1**4) / (zeta_2_check_Ert**2)
 
 # Define the expression to check against
 zeta_2 = theta * (
@@ -178,12 +205,29 @@ zeta_4 = (3*sp.exp(-2*kappa*t))/(zeta_2**2) * (
     + sigma**2 * (sigma - 4*kappa*rho)**2 * (2*kappa*theta + sigma**2)
 )
 
-# Check equivalence
-zeta_2_result = sp.simplify(zeta_2_check - zeta_2) == 0
-zeta_3_result = sp.simplify(zeta_3_check - zeta_3) == 0
-zeta_4_result = sp.simplify(zeta_4_check - zeta_4) == 0
-
 # Print result
-print("Are the zeta_2 equivalent?", zeta_2_result)
-print("Are the zeta_3 equivalent?", zeta_3_result)
-print("Are the zeta_4 equivalent?", zeta_4_result)
+print("Are zeta_2 and zeta_2_check equivalent?", sp.simplify(zeta_2_check - zeta_2) == 0)
+print("Are zeta_3 and zeta_3_check equivalent?", sp.simplify(zeta_3_check - zeta_3) == 0)
+print("Are zeta_4 and zeta_4_check equivalent?", sp.simplify(zeta_4_check - zeta_4) == 0)
+
+print("Are mu_2 and Ert2 equivalent?", sp.simplify(mu_2 - Ert2) == 0)
+print("Are mu_3 and Ert3 equivalent?", sp.simplify(mu_3 - Ert3) == 0)
+print("Are mu_4 and Ert4 equivalent?", sp.simplify(mu_4 - Ert4) == 0)
+
+# print("Are zeta_2 and zeta_2_check_Ert equivalent?", sp.simplify(zeta_2 - zeta_2_check_Ert) == 0)
+# print("Are zeta_3 and zeta_3_check_Ert equivalent?", sp.simplify(zeta_3 - zeta_3_check_Ert) == 0)
+# print("Are zeta_4 and zeta_4_check_Ert equivalent?", sp.simplify(zeta_4 - zeta_4_check_Ert) == 0)
+
+# print("Are zeta_2_check and zeta_2_check_Ert equivalent?", sp.simplify(zeta_2_check - zeta_2_check_Ert) == 0)
+# print("Are zeta_3_check and zeta_3_check_Ert equivalent?", sp.simplify(zeta_3_check - zeta_3_check_Ert) == 0)
+# print("Are zeta_4_check and zeta_4_check_Ert equivalent?", sp.simplify(zeta_4_check - zeta_4_check_Ert) == 0)
+
+# Difference between zetas and zetas_check
+# print(sp.simplify(zeta_3 - zeta_3_check)) # really long expression
+# print(sp.simplify(zeta_4 - zeta_4_check)) # an even longer expression
+
+# Numerical evaluation
+values = {mu: 0, theta: 0.19, kappa: 3, rho: -0.7, sigma: 0.4, t: 1}
+print(zeta_2.subs(values).evalf(), zeta_2_check.subs(values).evalf())
+print(zeta_3.subs(values).evalf(), zeta_3_check.subs(values).evalf())
+print(zeta_4.subs(values).evalf(), zeta_4_check.subs(values).evalf())
