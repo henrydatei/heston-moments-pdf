a81bfdd1db2316668c2c37ecbd92d55187629429
henrydatei <31316025+henrydatei@users.noreply.github.com>
2025-01-09 15:30:45 +0100

    kompletter parameter space, immer noch 1 Path. hoffentlich reichen die ressourcen

--- simulation_database/test_simulation.sh
+++ simulation_database/test_simulation.sh
@@ -3,8 +3,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=52
 #SBATCH --mem-per-cpu=9000
-#SBATCH --nodes=5
-#SBATCH --time=24:00:00
+#SBATCH --nodes=70
+#SBATCH --time=36:00:00
 #SBATCH -J fill_database_test
 #SBATCH --error="/home/s4307678/.out/myjob-%J.out"
 #SBATCH --output="/home/s4307678/.out/myjob-%J.out"
