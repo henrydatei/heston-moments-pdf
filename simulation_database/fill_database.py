e6e410ed6e1fb450f0da782ecc85a8110f7b352f
henrydatei <31316025+henrydatei@users.noreply.github.com>
2025-01-23 12:57:38 +0100

    mehr als 1 path geht nicht

--- simulation_database/fill_database.py
+++ simulation_database/fill_database.py
@@ -15,8 +15,6 @@ from simulation.utils import process_to_log_returns_interday
 from code_from_haozhe.RealizedMomentsEstimator_Aggregate_update import rMoments_mvsek, RM_NP_return
 from code_from_haozhe.RealizedSkewness_NP_MonthlyOverlap import rCumulants
 
-np.random.seed(0)
-
 results_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results')
 os.makedirs(results_dir, exist_ok=True)
 
@@ -32,7 +30,9 @@ time_points = 15 * 22 * 79 * 12
 burnin = 3 * 22 * 79 * 12
 T = 15
 S0 = 100
-paths = 100
+paths = 1
+seed = 0
+np.random.seed(seed)
 
 # Full Simulation
 v0_min = 0.01
@@ -62,17 +62,17 @@ mus = np.arange(mu_min, mu_max, mu_step)
 rhos = np.arange(rho_min, rho_max, rho_step)
 
 # Short local test
-# v0s = [0.1, 0.3]
-# kappas = [3]
-# thetas = [0.01, 0.5]
-# sigmas = [0.1, 0.5]
-# mus = [0]
-# rhos = [-0.7]
+# v0s = [0.11]
+# kappas = [0.61]
+# thetas = [0.66]
+# sigmas = [0.66]
+mus = [0]
+# rhos = [0.7]
 
 def create_simulation_and_save_it(params):
     subjob_id, start_date, end_date, time_points, T, S0, paths, v0, kappa, theta, sigma, mu, rho, burnin = params
     worker_id = os.getpid()
-    logging.info(f"Worker {worker_id}: Running simulation with v0={v0}, kappa={kappa}, theta={theta}, sigma={sigma}, mu={mu}, rho={rho}")
+    logging.info(f"Worker {worker_id}: Running simulation with v0={v0}, kappa={kappa}, theta={theta}, sigma={sigma}, mu={mu}, rho={rho}, seed={seed}")
 
     subjob_dir = os.path.join(results_dir, f'subjob_{subjob_id}')
     os.makedirs(subjob_dir, exist_ok=True)
@@ -109,15 +109,16 @@ def create_simulation_and_save_it(params):
             NP_rm3 REAL,
             NP_rm4 REAL,
             NP_rskewness REAL,
-            NP_rexcess_kurtosis REAL
+            NP_rexcess_kurtosis REAL,
+            seed INTEGER
         )
     ''')
     conn.commit()
     
     # check if this parameter combination is already in the database
-    c.execute('SELECT * FROM simulations WHERE v0 = ? AND kappa = ? AND theta = ? AND sigma = ? AND mu = ? AND rho = ? AND time_points = ? AND burnin = ? AND T = ? AND S0 = ? AND paths = ? AND start_date = ? AND end_date = ?', (v0, kappa, theta, sigma, mu, rho, time_points, burnin, T, S0, paths, start_date, end_date))
+    c.execute('SELECT * FROM simulations WHERE v0 = ? AND kappa = ? AND theta = ? AND sigma = ? AND mu = ? AND rho = ? AND time_points = ? AND burnin = ? AND T = ? AND S0 = ? AND paths = ? AND start_date = ? AND end_date = ? AND seed = ?', (v0, kappa, theta, sigma, mu, rho, time_points, burnin, T, S0, paths, start_date, end_date, seed))
     if c.fetchone() is not None:
-        logging.info(f'Worker {worker_id}: Simulation with parameters v0={v0}, kappa={kappa}, theta={theta}, sigma={sigma}, mu={mu}, rho={rho}, time_points={time_points}, burnin={burnin}, T={T}, S0={S0}, paths={paths}, start_date={start_date}, end_date={end_date} already exists.')
+        logging.info(f'Worker {worker_id}: Simulation with parameters v0={v0}, kappa={kappa}, theta={theta}, sigma={sigma}, mu={mu}, rho={rho}, time_points={time_points}, burnin={burnin}, T={T}, S0={S0}, paths={paths}, start_date={start_date}, end_date={end_date}, seed={seed} already exists.')
         return
 
     try:
@@ -142,7 +143,7 @@ def create_simulation_and_save_it(params):
             mu, kappa, theta, sigma, rho, v0, time_points, burnin, T, S0, paths,
             start_date, end_date, 'day', max_number_of_same_prices,
             realized_cumulants[0], realized_cumulants[1], realized_cumulants[2], realized_cumulants[3],
-            mvsek[0], mvsek[1], mvsek[2], mvsek[3], mvsek[4], mvsek[5]
+            mvsek[0], mvsek[1], mvsek[2], mvsek[3], mvsek[4], mvsek[5], seed
         )
 
         filename = os.path.join(subjob_dir, f'simulation_{worker_id}.csv')
@@ -154,7 +155,7 @@ def create_simulation_and_save_it(params):
         logging.info(f"Worker {worker_id} result: {result}")
     
         # insert into database
-        c.execute('INSERT INTO simulations (mu, kappa, theta, sigma, rho, v0, time_points, burnin, T, S0, paths, start_date, end_date, interval, max_number_of_same_prices, NP_rc1, NP_rc2, NP_rc3, NP_rc4, NP_rm1, NP_rm2, NP_rm3, NP_rm4, NP_rskewness, NP_rexcess_kurtosis) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', result)
+        c.execute('INSERT INTO simulations (mu, kappa, theta, sigma, rho, v0, time_points, burnin, T, S0, paths, start_date, end_date, interval, max_number_of_same_prices, NP_rc1, NP_rc2, NP_rc3, NP_rc4, NP_rm1, NP_rm2, NP_rm3, NP_rm4, NP_rskewness, NP_rexcess_kurtosis, seed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', result)
         conn.commit()
         conn.close()
         logging.info(f'Worker {worker_id}: Simulation saved in database.')
