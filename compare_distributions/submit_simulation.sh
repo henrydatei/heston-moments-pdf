69275797f49c1f230c87b7a16eac467107847c01
henrydatei <31316025+henrydatei@users.noreply.github.com>
2025-02-13 09:29:29 +0100

    request more power

--- compare_distributions/submit_simulation.sh
+++ compare_distributions/submit_simulation.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --mem-per-cpu=4000
 #SBATCH --nodes=1
-#SBATCH --time=01:00:00
+#SBATCH --time=24:00:00
 #SBATCH -J fill_database_with_distances
-#SBATCH --array=0-99
+#SBATCH --array=0-199
 #SBATCH --error="/home/s4307678/.out/myjob-%A_%a.out"
 #SBATCH --output="/home/s4307678/.out/myjob-%A_%a.out"
 
@@ -39,6 +39,6 @@ pip install tqdm
 # pip install yfinance
 # pip install pytorch-lightning
 
-srun python compare_distributions/fill_database_with_distances.py --i $SLURM_ARRAY_TASK_ID --chunks 100
+srun python compare_distributions/fill_database_with_distances.py --i $SLURM_ARRAY_TASK_ID --chunks 200
 
 deactivate
